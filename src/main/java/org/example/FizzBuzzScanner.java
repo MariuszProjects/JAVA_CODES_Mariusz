ff19dcb913ec873869a009a38be5916f976eed78
MariuszProject <mariusz_cz@interia.pl>
2023-05-06 16:44:38 +0200

    Examples of JAVA codes UPDATED

--- src/main/java/org/example/FizzBuzzScanner.java
+++ src/main/java/org/example/FizzBuzzScanner.java
@@ -1,22 +1,21 @@
 package org.example;
+import java.util.Scanner;
 
-public class FizzBuzz {
+public class FizzBuzzScanner {
     public static void main(String[] args) {
-      for (int number = 1; number <= 100; number++) {
+        Scanner enterNumber = new Scanner(System.in);
+        System.out.println("Please enter an integer: ");
+        int number = enterNumber.nextInt();
             if (number % 15 == 0) {
                 System.out.println("FizzBuzz - the integer can be divided by 3 & 5");
-                continue;
             }
-            if (number % 3 == 0) {
+            else if (number % 3 == 0) {
                 System.out.println("Fizz - the integer can be divided by 3");
-                continue;
             }
-            if (number % 5 == 0) {
+            else if (number % 5 == 0) {
                 System.out.println("Buzz - the integer can be divided by 5");
             }
             else System.out.println(number);
         }
     }
-}
-
 
